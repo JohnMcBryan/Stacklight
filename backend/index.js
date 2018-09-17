478488e64bbccc64c8cbb40501f2feea2f624b2d
Nicole D'Annibale <nid219@lehigh.edu>
2018-09-17 16:26:40 -0400

    added livechat code from last semester

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var usernames = {};
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        delete usernames[socket.username];
+        io.sockets.emit('updateusers', usernames);
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+    });
+});
+
+io.on('adduser', function(username){
+    socket.username = username;
+    usernames[username] = username;
+    socket.emit('updatechat', 'SERVER', 'you have connected');
+    socket.broadcoast.emit('updatechat', 'SERVER', username + 'has connected');
+    io.sockets.emit('updateusers', username);
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+});
+
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+  });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
