370a750e14e7678f32c60dcfcf7b26c1d67f5d13
Mira Straathof <mts219@lehigh.edu>
2018-09-17 16:42:08 -0400

    2_Mira_Add_Project_Page
    - Add the project page. On load, still goes to to index.html.
    - Add functionality to stoplight button to go to all projects page

--- web/ldeploy.sh
+++ web/ldeploy.sh
@@ -21,21 +21,25 @@ mkdir $TARGETIMAGEFOLDER
 cp index.html $TARGETFOLDER
 cp style.css $TARGETFOLDER
 cp node_modules/jquery/dist/jquery.min.js $TARGETFOLDER
+cp allProjectsPage.html $TARGETFOLDER
+cp allProjectsPage.js $TARGETFOLDER
 cp Images/logo.png $TARGETIMAGEFOLDER
 cp Images/logoWhite.png $TARGETIMAGEFOLDER
 cp Images/uploadwhite.png $TARGETIMAGEFOLDER
 cp Images/add-list.png $TARGETIMAGEFOLDER
+cp Images/newproject.png $TARGETIMAGEFOLDER
+cp Images/project.png $TARGETIMAGEFOLDER
 
 # step 4: compile TypeScript files
 node_modules/.bin/tsc app.ts --strict --outFile $TARGETFOLDER/app.js
 
 # step 5: compile tests and copy tests to the local deploy folder
-node_modules/.bin/tsc apptest.ts --strict --outFile $TARGETFOLDER/apptest.js
-cp spec_runner.html $TARGETFOLDER
-cp node_modules/jasmine-core/lib/jasmine-core/jasmine.css $TARGETFOLDER
-cp node_modules/jasmine-core/lib/jasmine-core/jasmine.js $TARGETFOLDER
-cp node_modules/jasmine-core/lib/jasmine-core/boot.js $TARGETFOLDER
-cp node_modules/jasmine-core/lib/jasmine-core/jasmine-html.js $TARGETFOLDER
+# node_modules/.bin/tsc apptest.ts --strict --outFile $TARGETFOLDER/apptest.js
+# cp spec_runner.html $TARGETFOLDER
+# cp node_modules/jasmine-core/lib/jasmine-core/jasmine.css $TARGETFOLDER
+# cp node_modules/jasmine-core/lib/jasmine-core/jasmine.js $TARGETFOLDER
+# cp node_modules/jasmine-core/lib/jasmine-core/boot.js $TARGETFOLDER
+# cp node_modules/jasmine-core/lib/jasmine-core/jasmine-html.js $TARGETFOLDER
 
 # step 6: launch the server.  Be sure to disable caching
 # (Note: we don't currently use -s for silent operation)
