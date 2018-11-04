d227420ea341377f1b6146d58a2ca4027e51836e
rsg219 <rsg219@lehigh.edu>
2018-11-03 20:33:37 -0400

    Added project list to profile

--- web/js/profile.js
+++ web/js/profile.js
@@ -10,4 +10,14 @@ $(function () {
             $(".bodyDiv").append("<div>" + localStorage.getItem("email") + "</div>")
         },
     });
+    $.ajax({
+         type: "GET",
+         url: backendUrl + "/projects/all/" + localStorage.getItem("email"),
+         dataType: "json",
+         success: function (result){
+            for (var i = 0; i < result.mProjectData.length; ++i) {
+                       $(".projects").append("<div>" + result.mProjectData[i].mName + "</div>");
+            }
+         },
+    });
 });
